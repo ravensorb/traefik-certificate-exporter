e21c20d7bd2d4c11bb7acb28e09fe6497e087b79
Shawn Anderson <sanderson@eye-catcher.com>
2022-01-15 21:34:51 -0500

    Cleanup

--- build-test.sh
+++ build-test.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
-ACTION=${1:publish}
+ACTION=${1:-}
+
+rm -r dist
 
 python3 -m build
 
 if [ "$ACTION" == "publish" ]; then
-    python3 -m twine upload --repository pypi dist/*
+    python3 -m twine upload --repository testpypi dist/*
 
     cd docker
-    docker build -f Dockerfile.test .
+    docker build -f Dockerfile.test . --build-arg TEST_VERSION=1 --no-cache
     cd ..
 fi
